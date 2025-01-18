04a71a3e040ca7d06248893e7240cd98a2ff1832
Cauã de Lima <caua-l1@outlook.com>
2025-01-18 14:40:20 -0300

    Adicionado Header + Adicionado Logout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import drop from '../../assets/icons/Drop.svg'
+import { jwtDecode } from "jwt-decode";
 import { HeaderContext } from '../../Contexts/HeaderContext.jsx';
 import styles from './Header.module.css';
 
 const Header = () => {
-  const { content } = useContext(HeaderContext)
+  const { content } = useContext(HeaderContext);
+  const [userData, setUserData] = useState();
+  const [dropDown, setDropDown] = useState(false);
+
+  function handleLogout() {
+    localStorage.removeItem('userToken');
+    window.location.href = '/';
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('userToken');
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      setUserData(decodedToken);
+    }
+  }, []);
 
   return (
     <header className={styles.header}>
       <h2>{content}</h2>
 
-      <div>
-        <div>C</div>
-        <div>
-          <h3>Nome de usuário</h3>
-          <p>example@example.com</p>
+      <div className={styles.account}>
+        <div className={styles.details}>
+          <div>{userData && userData.username[0].toUpperCase()}</div>
+
+          <div>
+              <h3>{userData ? userData.username : 'Carregando...'}</h3>
+              <p>{userData ? userData.user_email : '...'}</p>
+          </div>
         </div>
+
+        <button onClick={handleLogout} className={styles.logout}>Logout</button>
       </div>
     </header>
   )
